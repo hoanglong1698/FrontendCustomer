b8ae706dc3a8e438f190179ee9c557b5d1d8a707
Hoang Long <1612352@student.hcmus.edu.vn>
2020-07-29 10:25:49 +0700

    Giao diện tạo tài khoản khách hàng

---
+++ src/components/Employee/CreateCustomerAccount.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="register">
+    <b-container>
+      <div class="form">
+        <b-form @submit="onSubmit">
+          <b-form-group>
+            <b-spinner v-if="isLoading" label="Loading..."></b-spinner>
+            <b-alert v-if="isSucceed && isRegister" variant="success" show>Tạo tài khoản thành công</b-alert>
+            <b-alert
+              v-if="!isSucceed && isRegister"
+              variant="danger"
+              show
+            >Tạo tài khoản thất bại, vui lòng kiểm tra lại thông tin đã nhập</b-alert>
+          </b-form-group>
+
+          <b-form-group label="Họ tên">
+            <b-input-group class="mb-2">
+              <b-input-group-prepend is-text>
+                <b-icon icon="person-lines-fill"></b-icon>
+              </b-input-group-prepend>
+              <b-form-input v-model="form.name" placeholder="Nguyen Van A" required></b-form-input>
+            </b-input-group>
+          </b-form-group>
+
+          <b-form-group label="Email">
+            <b-input-group class="mb-2">
+              <b-input-group-prepend is-text>
+                <b-icon icon="envelope-fill"></b-icon>
+              </b-input-group-prepend>
+              <b-form-input v-model="form.email" placeholder="nguyenvana@gmail.com" required></b-form-input>
+            </b-input-group>
+          </b-form-group>
+
+          <b-form-group label="Số điện thoại">
+            <b-input-group class="mb-2">
+              <b-input-group-prepend is-text>
+                <b-icon icon="phone"></b-icon>
+              </b-input-group-prepend>
+              <b-form-input v-model="form.phone" type="number" placeholder="0123456789" required></b-form-input>
+            </b-input-group>
+          </b-form-group>
+
+          <b-form-group label="Tên chủ thẻ">
+            <b-input-group class="mb-2">
+              <b-input-group-prepend is-text>
+                <b-icon icon="credit-card"></b-icon>
+              </b-input-group-prepend>
+              <b-form-input v-model="form.cardname" placeholder="Nguyen Van A" required></b-form-input>
+            </b-input-group>
+          </b-form-group>
+
+          <div class="d-flex justify-content-center">
+            <b-button type="submit" variant="primary">Tạo tài khoản</b-button>
+          </div>
+
+          <div class="m-2">
+            <b-button v-if="isSucceed" v-b-modal.modal-prevent-closing>Xem thông tin tài khoản vừa tạo</b-button>
+
+            <b-modal
+              id="modal-prevent-closing"
+              ref="modal"
+              title="Kết quả đăng ký"
+              @show="resetModal"
+              @hidden="resetModal"
+              @ok="handleOk"
+            >
+              <b-row>
+                <b-col sm="3">
+                  <h6 class="mt-0">Trạng thái:</h6>
+                </b-col>
+                <b-col sm="9">
+                  <p>{{ModalCreateCustomerMessage}}</p>
+                </b-col>
+              </b-row>
+
+              <b-row>
+                <b-col sm="3">
+                  <h6 class="mt-0">Username:</h6>
+                </b-col>
+                <b-col sm="9">
+                  <p>{{ModalCreateCustomerUsername}}</p>
+                </b-col>
+              </b-row>
+
+              <b-row>
+                <b-col sm="3">
+                  <h6 class="mt-0">Password:</h6>
+                </b-col>
+                <b-col sm="9">
+                  <p>{{ModalCreateCustomerPassword}}</p>
+                </b-col>
+              </b-row>
+
+              <b-row>
+                <b-col sm="3">
+                  <h6 class="mt-0">Số thẻ:</h6>
+                </b-col>
+                <b-col sm="9">
+                  <p>{{ModalCreateCustomerCardNumber}}</p>
+                </b-col>
+              </b-row>
+
+              <b-row>
+                <b-col sm="3">
+                  <h6 class="mt-0">Tên chủ thẻ:</h6>
+                </b-col>
+                <b-col sm="9">
+                  <p>{{ModalCreateCustomerCardName}}</p>
+                </b-col>
+              </b-row>
+            </b-modal>
+          </div>
+        </b-form>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        phone: "",
+        cardname: ""
+      },
+      isRegister: false,
+      isLoading: false
+    };
+  },
+
+  computed: {
+    ...mapGetters([
+      "isSucceed",
+      "ErrorMessage",
+      "ModalCreateCustomerMessage",
+      "ModalCreateCustomerUsername",
+      "ModalCreateCustomerPassword",
+      "ModalCreateCustomerCardNumber",
+      "ModalCreateCustomerCardName"
+    ])
+  },
+
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      const customer = {
+        email: this.form.email,
+        name: this.form.name,
+        phone: this.form.phone,
+        cardName: this.form.cardname,
+        adminId: 1
+      };
+      this.$store.dispatch("createCustomer", customer);
+      this.isLoading = true;
+
+      setTimeout(() => {
+        this.isRegister = true;
+         this.isLoading = false;
+      }, 3000);
+      setTimeout(() => {
+        this.isRegister = false;
+      }, 7000);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.form {
+  margin: auto;
+  width: 350px;
+}
+.register {
+  position: relative;
+}
+</style>
