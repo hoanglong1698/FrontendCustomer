1a19f3728a4851b385d2ca15f29737ac93413f35
Hoang Long <1612352@student.hcmus.edu.vn>
2020-07-29 10:26:21 +0700

    Component table

---
+++ src/components/common/Table.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-table
+      striped
+      hover
+      selectable
+      outlined
+      fixed="true"
+      :select-mode="selectMode"
+      :items="items"
+      :fields="fields"
+      @row-selected="onRowSelected"
+      responsive="sm"
+      sticky-header
+      head-variant="dark"
+    ></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items", "fields"],
+  data() {
+    return {
+      selectMode: "single",
+    };
+  },
+  methods: {
+    onRowSelected(items) {
+      alert(JSON.stringify(items));
+    },
+  },
+};
+</script>
