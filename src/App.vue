861c98cdaad78f0df81daf0db0157166f78e2628
Hoang Long <1612352@student.hcmus.edu.vn>
2020-07-29 10:28:23 +0700

    Store

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+
+<script>
+export default {
+  mounted() {
+    // Clear the browser cache data in localStorage when closing the browser window
+    window.onbeforeunload = function () {
+      var storage = window.localStorage;
+      storage.clear();
+    };
+  },
+};
+</script>
 
 <style>
 #app {
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,188 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    ErrorMessage: '',
+    isSucceed: false,
+    TotalBalance: '',
+    ModalCreateCustomerMessage: '',
+    ModalCreateCustomerUsername: '',
+    ModalCreateCustomerPassword: '',
+    ModalCreateCustomerCardNumber: '',
+    ModalCreateCustomerCardName: '',
+    correctAuthInfo: true,
+    accessToken: '',
+    role: '',
+    Transactions: [],
   },
+
+  getters: {
+    ErrorMessage(state) {
+      return state.ErrorMessage;
+    },
+    TotalBalance(state) {
+      return state.TotalBalance;
+    },
+    Transactions(state) {
+      return state.Transactions;
+    },
+    isSucceed(state) {
+      return state.isSucceed
+    },
+    ModalCreateCustomerMessage(state) {
+      return state.ModalCreateCustomerMessage;
+    },
+    ModalCreateCustomerUsername(state) {
+      return state.ModalCreateCustomerUsername;
+    },
+    ModalCreateCustomerPassword(state) {
+      return state.ModalCreateCustomerPassword;
+    },
+    ModalCreateCustomerCardNumber(state) {
+      return state.ModalCreateCustomerCardNumber;
+    },
+    ModalCreateCustomerCardName(state) {
+      return state.ModalCreateCustomerCardName;
+    },
+    CorrectAuthInfo(state) {
+      return state.correctAuthInfo;
+    },
+    Role(state) {
+      return state.role;
+    },
+  },
+
   mutations: {
+    IS_SUCCEED(state, payload) {
+      state.isSucceed = payload;
+    },
+
+    ERROR_MESSAGE(state, payload) {
+      state.ErrorMessage = payload;
+    },
+
+    TOTAL_BALANCE(state, payload) {
+      state.TotalBalance = payload;
+    },
+
+    TRANSACTIONS(state, payload) {
+      state.Transactions = payload;
+    },
+
+    MODAL_CREATE_CUSTOMER_MESSAGE(state, payload) {
+      state.ModalCreateCustomerMessage = payload;
+    },
+
+    MODAL_CREATE_CUSTOMER_USERNAME(state, payload) {
+      state.ModalCreateCustomerUsername = payload;
+    },
+
+    MODAL_CREATE_CUSTOMER_PASSWORD(state, payload) {
+      state.ModalCreateCustomerPassword = payload;
+    },
+
+    MODAL_CREATE_CUSTOMER_CARDNUMBER(state, payload) {
+      state.ModalCreateCustomerCardNumber = payload;
+    },
+
+    MODAL_CREATE_CUSTOMER_CARDNAME(state, payload) {
+      state.ModalCreateCustomerCardName = payload;
+    },
+
+    LOGIN(state) {
+      state.accessToken = localStorage.getItem('access_token');
+      state.role = localStorage.getItem('role');
+    },
   },
   actions: {
+    async login(ctx, loginInfo) {
+      await axios.post('https://i-banking.herokuapp.com/lh-bank/authenticate', {
+        userName: loginInfo.userName,
+        password: loginInfo.password
+      })
+        .then(function (response) {
+          localStorage.setItem('access_token', response.data.bearerToken);
+          localStorage.setItem('role', response.data.role);
+          ctx.commit('LOGIN');
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+
+    //nạp tiền vào tài khoản
+    async payIn(ctx, data) {
+      await axios.post('https://i-banking.herokuapp.com/lh-bank/deposit', data, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+        }
+      })
+        .then(function (response) {
+          console.log('so du:', response.data.data.totalBalance);
+          ctx.commit('IS_SUCCEED', true);
+          ctx.commit('TOTAL_BALANCE', response.data.data.totalBalance);
+        })
+        .catch(function (error) {
+          ctx.commit('IS_SUCCEED', false);
+          console.log(error);
+        });
+    },
+
+    async createCustomer(ctx, customer) {
+      await axios.post('https://i-banking.herokuapp.com/lh-bank/register', customer, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(function (response) {
+          console.log(response.data);
+          ctx.commit('IS_SUCCEED', true);
+          ctx.commit('MODAL_CREATE_CUSTOMER_MESSAGE', response.data.message);
+          ctx.commit('MODAL_CREATE_CUSTOMER_USERNAME', response.data.data.userName);
+          ctx.commit('MODAL_CREATE_CUSTOMER_PASSWORD', response.data.data.password);
+          ctx.commit('MODAL_CREATE_CUSTOMER_CARDNUMBER', response.data.data.account.cardNumber);
+          ctx.commit('MODAL_CREATE_CUSTOMER_CARDNAME', response.data.data.account.cardName);
+        })
+        .catch(function (error) {
+          ctx.commit('IS_SUCCEED', false);
+          ctx.commit('MODAL_CREATE_CUSTOMER_MESSAGE', 'Thất bại');
+          console.log(error);
+        });
+    },
+
+    async getTransactions(ctx, data) {
+      var url = 'https://i-banking.herokuapp.com/lh-bank/get-transactions/';
+      if (data.type == "Nhận tiền") {
+        url = url + 'receiver/' + data.cardNumber;
+      }
+      if (data.type == "Chuyển tiền") {
+        url = url + 'send/' + data.cardNumber;
+      }
+      if (data.type == "Nhắc nợ") {
+        url = url + 'debt/' + data.cardNumber;
+      }
+
+      await axios.get(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+        }
+      })
+        .then(function (response) {
+          console.log('transactions:', response.data.data.transactions);
+          ctx.commit('IS_SUCCEED', true);
+          ctx.commit('TRANSACTIONS', response.data.data.transactions);
+        })
+        .catch(function (error) {
+          ctx.commit('IS_SUCCEED', false);
+          console.log(error);
+        });
+    },
   },
   modules: {
   }
