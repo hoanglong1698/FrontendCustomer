2fbf2ad3d450aa6b0616310191ac7905ac25b733
Hoang Long <1612352@student.hcmus.edu.vn>
2020-07-29 10:28:06 +0700

    Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,55 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () =>
+      import( /* webpackChunkName: "login" */ '../views/Login.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/employee',
+    name: 'employee',
+    meta: { requiresAuth: true },
+    component: () =>
+      import( /* webpackChunkName: "employee" */ '../views/Employee/Employee.vue'),
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    meta: { requiresAuth: true },
+    component: () =>
+      import( /* webpackChunkName: "employee" */ '../views/Admin/Admin.vue'),
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () =>
+      import( /* webpackChunkName: "login" */ '../views/Login.vue'),
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!localStorage.access_token) {
+      next({
+        path: '/login',
+        query: { retUrl: to.fullPath }
+      })
+    }
+
+    else {
+      next()
+    }
+  }
+  else next()
 })
 
 export default router
