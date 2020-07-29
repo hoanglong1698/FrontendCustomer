ec679a24e16d76bb18ae6526ea2c23e88ba34f66
Hoang Long <1612352@student.hcmus.edu.vn>
2020-07-29 17:04:56 +0700

    Chức năng cập nhật thông tin employee

--- src/components/Admin/ManageEmployee.vue
+++ src/components/Admin/ManageEmployee.vue
@@ -3,17 +3,36 @@
     <div class="form">
       <b-form>
         <b-form-group>
-          <b-alert v-if="isSucceed && isDel" variant="success" show>Xóa tài khoản thành công</b-alert>
-          <b-alert v-if="!isSucceed && isDel" variant="danger" show>Xóa tài khoản thất bại</b-alert>
+          <b-alert v-if="isSucceed && isDeleled" variant="success" show>Xóa tài khoản thành công</b-alert>
+          <b-alert v-if="!isSucceed && isDeleled" variant="danger" show>Xóa tài khoản thất bại</b-alert>
         </b-form-group>
-
-        <b-button style="width: 300px" v-b-toggle.collapse-1 variant="outline-danger">Xóa nhân viên</b-button>
-        <b-collapse id="collapse-1" class="mt-2">
+        <b-button
+          v-if="showDelete"
+          style="width: 300px"
+          v-b-toggle.collapse-1
+          variant="outline-danger"
+          @click="onClickDelete"
+        >Xóa nhân viên</b-button>
+        <b-collapse id="collapse-1" class="mt-2" v-model="visibleDelete">
           <b-form-group label="ID nhân viên">
             <b-form-input v-model="id" type="number" readonly placeholder="ID nhân viên"></b-form-input>
           </b-form-group>
 
-          <b-button style="width: 300px" class="mb-4" type="button" variant="danger" @click="onDelete">Xóa</b-button>
+          <b-button
+            v-b-modal.modal-confirm
+            style="width: 300px"
+            class="mb-4"
+            type="button"
+            variant="danger"
+          >Xóa</b-button>
+          <div class="m-2">
+            <b-modal
+              id="modal-confirm"
+              ref="modal"
+              title="Bạn có chắc chắn muốn làm điều này?"
+              @ok="onConfirmDelete"
+            >Xóa nhân viên ID: {{this.id}} - {{this.name}}</b-modal>
+          </div>
         </b-collapse>
       </b-form>
     </div>
@@ -25,43 +44,58 @@
         </b-form-group>
 
         <b-button
+          @click="onClickUpdate"
+          v-if="showUpdate"
           style="width: 300px"
           v-b-toggle.collapse-2
           variant="outline-success"
         >Cập nhật thông tin nhân viên</b-button>
-        <b-collapse id="collapse-2" class="mt-2">
+        <b-collapse id="collapse-2" class="mt-2" v-model="visibleUpdate">
           <b-form-group label="ID nhân viên">
             <b-form-input v-model="id" type="number" readonly placeholder="ID nhân viên"></b-form-input>
           </b-form-group>
 
-          <b-form-group label="Họ tên đầy đủ">
-            <b-form-input v-model="fullname" type="text" placeholder="Họ tên đầy đủ"></b-form-input>
+          <b-form-group label="Họ tên">
+            <b-input-group class="mb-2">
+              <b-input-group-prepend is-text>
+                <b-icon icon="person-lines-fill"></b-icon>
+              </b-input-group-prepend>
+              <b-form-input v-model="name" placeholder="Nguyen Van A" required></b-form-input>
+            </b-input-group>
           </b-form-group>
 
           <b-form-group label="Email">
-            <b-form-input v-model="email" type="text" placeholder="Email"></b-form-input>
+            <b-input-group class="mb-2">
+              <b-input-group-prepend is-text>
+                <b-icon icon="envelope-fill"></b-icon>
+              </b-input-group-prepend>
+              <b-form-input v-model="email" placeholder="nguyenvana@gmail.com" required></b-form-input>
+            </b-input-group>
           </b-form-group>
 
           <b-form-group label="Số điện thoại">
-            <b-form-input v-model="phone" type="text" placeholder="Số điện thoại"></b-form-input>
-          </b-form-group>
-
-          <b-form-group label="Ngày sinh">
-            <b-form-input
-              v-model="dob"
-              :text="dob"
-              placeholder="ngày/tháng/năm"
-              class="mb-2"
-              readonly
-            ></b-form-input>
+            <b-input-group class="mb-2">
+              <b-input-group-prepend is-text>
+                <b-icon icon="phone"></b-icon>
+              </b-input-group-prepend>
+              <b-form-input v-model="phone" type="number" placeholder="0123456789" required></b-form-input>
+            </b-input-group>
           </b-form-group>
 
           <b-button style="width: 300px" type="button" variant="success" @click="onUpdate">Cập nhật</b-button>
         </b-collapse>
       </b-form>
     </div>
+    <b-spinner class="mt-3" v-if="isLoading" label="Loading..."></b-spinner>
     <div style="margin-top: 50px">
-      <h6>Danh sách nhân viên ngân hàng</h6>
+      <b-button
+        v-if="showRefresh"
+        style="width: 300px"
+        v-b-toggle.collapse-1
+        variant="outline-primary"
+        @click="onRefresh"
+      >Làm mới danh sách nhân viên</b-button>
+      <h5 class="mt-2 mb-2">Danh sách nhân viên ngân hàng</h5>
       <b-table
         striped
         hover
@@ -85,14 +119,19 @@ export default {
   data() {
     return {
       id: "",
-      fullname: "",
+      name: "",
       email: "",
       phone: "",
-      dob: "",
       isUpdate: false,
-      isDel: false,
+      isDeleled: false,
+      isLoading: false,
       selectMode: "single",
       Fields: [],
+      showRefresh: true,
+      showDelete: true,
+      showUpdate: true,
+      visibleDelete: false,
+      visibleUpdate: false,
     };
   },
 
@@ -140,46 +179,66 @@ export default {
   methods: {
     onRowSelected(items) {
       this.id = items[0].id;
-      this.fullname = items[0].full_name;
+      this.name = items[0].name;
       this.email = items[0].email;
       this.phone = items[0].phone;
-      this.dob = items[0].dob;
     },
 
     onUpdate() {
+      this.isLoading = true;
       const info = {
-        ID: this.id,
-        FullName: this.fullname,
-        Email: this.email,
-        Phone: this.phone,
-        DoB: this.dob,
+        id: this.id,
+        email: this.email,
+        name: this.name,
+        phone: this.phone,
+        password: "123456",
+        action: "UPDATE",
       };
-      alert(JSON.stringify(info));
-      //this.$store.dispatch("updateEmployee", info);
+      this.$store.dispatch("updateEmployee", info);
+      
       setTimeout(() => {
+        this.$store.dispatch("getListEmployee");
         this.isUpdate = true;
+        this.isLoading = false;
       }, 3000);
       setTimeout(() => {
         this.isUpdate = false;
+        this.isLoading = false;
       }, 6000);
     },
 
-    onDelete() {
-      const info = {
-        id: this.id,
-        FullName: this.fullname,
-        Email: this.email,
-        Phone: this.phone,
-        DoB: this.dob,
-      };
-      console.log(info);
-      //this.$store.dispatch("deleteEmployee", this.id);
+    onConfirmDelete() {
+      this.isLoading = true;
+      this.$store.dispatch("deleteEmployee", this.id);
       setTimeout(() => {
-        this.isDel = true;
+        this.$store.dispatch("getListEmployee");
+        this.isDeleled = true;
+        this.isLoading = false;
       }, 3000);
       setTimeout(() => {
-        this.isDel = false;
+        this.isDeleled = false;
+        this.isLoading = false;
       }, 6000);
+    },
+
+    onRefresh() {
+      this.isLoading = true;
+      this.$store.dispatch("getListEmployee");
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2000);
+    },
+
+    onClickDelete() {
+      this.showRefresh = !this.showRefresh;
+      this.visibleDelete = !this.visibleDelete;
+      this.showUpdate = !this.showUpdate;
+    },
+
+    onClickUpdate() {
+      this.showRefresh = !this.showRefresh;
+      this.visibleUpdate = !this.visibleUpdate;
+      this.showDelete = !this.showDelete;
     },
   },
 };
