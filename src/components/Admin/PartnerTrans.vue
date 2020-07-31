e2c500866c36a927b5a1f1c8c76659fd0bc9c87c
Hoang Long <1612352@student.hcmus.edu.vn>
2020-07-31 22:05:39 +0700

    Chức năng xem lịch sử giao dịch với đối tác

--- src/components/Admin/PartnerTrans.vue
+++ src/components/Admin/PartnerTrans.vue
@@ -1,94 +1,135 @@
 <template>
-  <div class="form">
-    <br />
-    <h5>Tất cả giao dịch với ngân hàng đối tác</h5>
-    <Table :items="AllTrans"></Table>
-    <b-button variant="primary" @click="loadAll">Xem</b-button>
-
-
-    <div id="time" style="margin-top: 100px">
-      <h5>Xem theo khoảng thời gian</h5>
-      <b-form-group label="Từ ngày:">
-        <b-form-datepicker v-model="from" class="mb-2"></b-form-datepicker>
+  <div>
+    <div id="time" class="form">
+      <b-form-group label="Từ ngày">
+        <b-form-datepicker v-model="from" class="mb-2" placeholder="Bấm để chọn ngày"></b-form-datepicker>
       </b-form-group>
 
-      <b-form-group label="Đến ngày:">
-        <b-form-datepicker v-model="to" class="mb-2"></b-form-datepicker>
+      <b-form-group label="Đến ngày">
+        <b-form-datepicker v-model="to" class="mb-2" placeholder="Bấm để chọn ngày"></b-form-datepicker>
       </b-form-group>
-      <b-button variant="primary" @click="loadFromTimeToTime">Xem</b-button>
-    </div>
-    <br />
-    <Table :items="TransFromTimeToTime"></Table>
 
-    <div id="partner" style="margin-top: 100px">
-      <h5>Xem theo đối tác</h5>
-      <b-form-group>
+      <b-form-group label="Chọn đối tác">
         <b-form-select v-model="partner" :options="options"></b-form-select>
       </b-form-group>
-      <b-button variant="primary" @click="loadPartner">Xem</b-button>
+      <b-button variant="primary" @click="loadPartnerTransaction">Xem</b-button>
     </div>
     <br />
-    <Table :items="TransPartner"></Table>
 
-    <div id="amount" style="margin-top: 100px">
-      <h5>Tổng số tiền đã giao dịch với đối tác</h5>
-      <b-form-group inline>
-        <b-form-select v-model="amoutPartner" :options="options"></b-form-select>
-      </b-form-group>
+    <b-spinner v-if="isLoading" label="Loading..."></b-spinner>
 
-      <b-form-group>
-        <b-button variant="primary" @click="loadAmount">Xem</b-button>
-      </b-form-group>
-
-      <b-form-group>
-        <b-form-input readonly :value="PartnerMoney" placeholder="0 VND"></b-form-input>
-      </b-form-group>
+    <div v-if="isSucceed && isClicked">
+      <h5>Lịch sử giao dịch</h5>
+      <Table :items="PartnerTransactions" :fields="Fields"></Table>
+      <div id="amount" class="form mt-4 mb-5">
+        <h5>Tổng số tiền đã giao dịch với đối tác</h5>
+        <b-form-input readonly :value="TotalAmount + ' VND'" placeholder="0 VND"></b-form-input>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Table from "../common/Table.vue";
+import { mapGetters } from "vuex";
 
 export default {
   components: {
-    Table
+    Table,
   },
   data() {
     return {
-      partner: "",
-      amoutPartner: "",
+      partner: "0",
       from: "",
       to: "",
       options: [
-        { value: "", text: "Chọn ngân hàng" },
-        { value: "nganhanga", text: "Ngân hàng A" },
-        { value: "nganhangb", text: "Ngân hàng B" }
-      ]
+        { value: "0", text: "Tất cả" },
+        { value: "1", text: "Ngân hàng pgpbank" },
+        { value: "3", text: "Ngân hàng HHL Bank" },
+      ],
+      isLoading: false,
+      Fields: [],
+      isClicked: false,
     };
   },
+
   mounted() {
-    //this.$store.dispatch("partnerTrans");
+    this.Fields = [
+      {
+        key: "senderCard",
+        label: "STK gửi",
+        sortable: false,
+      },
+      {
+        key: "receiverCard",
+        label: "STK nhận",
+        sortable: false,
+      },
+      {
+        key: "amount",
+        label: "Số tiền",
+        sortable: false,
+      },
+      {
+        key: "content",
+        label: "Nội dung",
+        sortable: false,
+      },
+      {
+        key: "createDate",
+        label: "Ngày tạo",
+        sortable: true,
+      },
+      {
+        key: "merchantName",
+        label: "Tên ngân hàng",
+        sortable: true,
+      },
+    ];
   },
-  computed: {},
+
+  computed: {
+    ...mapGetters([
+      "isSucceed",
+      "ErrorMessage",
+      "PartnerTransactions",
+      "TotalAmount",
+    ]),
+  },
+
   methods: {
-    loadAll() {
-      //this.$store.dispatch("partnerTrans");
+    loadPartnerTransaction() {
+      if (this.from >= this.to) {
+        alert("Sai ngày bắt đầu. Vui lòng chọn lại");
+        return;
+      }
+
+      this.isLoading = true;
+      this.isClicked = true;
+
+      setTimeout(() => {
+        this.isLoading = false;
+        this.$store.dispatch("getPartnerTransaction", {
+          from: this.from,
+          to: this.to,
+          id: this.partner,
+        });
+        if (this.isSucceed === true) {
+          this.$bvToast.toast("Xem chi tiết tại bảng lịch sử giao dịch", {
+            title: `Lấy dữ liệu thành công`,
+            variant: "success",
+            solid: true,
+          });
+        } else {
+          this.$bvToast.toast("Vui lòng thử lại sau", {
+            title: `Lấy dữ liệu thất bại`,
+            variant: "danger",
+            solid: true,
+          });
+        }
+      }, 1500);
     },
-    loadFromTimeToTime() {
-      //   this.$store.dispatch("partnerTransByTime", {
-      //     from: this.from,
-      //     to: this.to
-      //   });
-    },
-    loadPartner() {
-      //this.$store.dispatch("partnerTransByName", this.bank1);
-      alert(this.partner);
-    },
-    loadAmount() {
-      //this.$store.dispatch("partnerStatisticMoney", this.bank2);
-    }
-  }
+  },
 };
 </script>
 
